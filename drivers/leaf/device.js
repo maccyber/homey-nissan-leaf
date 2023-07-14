8fbd4a79db0d84234c54deb4b0d6b4fefb243d37
Jonas Enge <jonas.maccyber.enge@nav.no>
2023-07-14 16:41:03 +0200

    Adds RemoteAC

--- drivers/leaf/device.js
+++ drivers/leaf/device.js
@@ -11,11 +11,13 @@ class LeafDevice extends Device {
     const [username, password, pollInterval, regionCode] = this.getSettings();
     this.updateCapabilities(username, password, regionCode);
     this.homey.setInterval(() => this.updateCapabilities(username, password, regionCode), pollInterval);
+
     this.registerCapabilityListener('onoff.climate_control', async (value) => {
       const client = await leafConnect({ username, password, regionCode });
       this.log(await client.climateControlStatus());
       return value;
     });
+
     this.registerCapabilityListener('onoff.charging', async (value) => {
       return false;
       // const client = await leafConnect({ username, password, regionCode });
@@ -26,17 +28,28 @@ class LeafDevice extends Device {
     try {
       const client = await leafConnect({ username, password, regionCode });
       this.log(client.sessionInfo());
+
       const status = await client.cachedStatus();
+      this.log(status);
+
       const climateControlStatus = await client.climateControlStatus();
       this.log(climateControlStatus);
+      const racr = climateControlStatus.RemoteACRecords;
+      const acIsRunning = racr.length > 0
+        && racr.OperationResult !== null
+        && racr.OperationResult.toString().startsWith('START')
+        && racr.RemoteACOperation === 'START';
+      this.setCapabilityValue('onoff.climate_control', acIsRunning);
+
       const { BatteryStatusRecords: { BatteryStatus, PluginState } } = status;
 
       const isCharging = BatteryStatus.BatteryChargingStatus !== 'NOT_CHARGING';
       const isConnected = PluginState !== 'NOT_CONNECTED';
-      this.log(status)
+
       this.setCapabilityValue('measure_battery', Number(BatteryStatus.SOC.Value)).catch(this.error);
       this.setCapabilityValue('is_charging', isCharging).catch(this.error);
       this.setCapabilityValue('is_connected', isConnected).catch(this.error);
+      this.setCapabilityValue('onoff.charging', isConnected && !isCharging).catch(this.error);
     } catch (error) {
       this.error(error);
     }
